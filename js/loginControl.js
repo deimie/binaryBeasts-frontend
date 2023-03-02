de3c88a1bebbd1aca14c0cb0a8d3e3aa34d75340
deim <evanjoelsanchez@gmail.com>
2023-03-02 11:56:31 -0800

    Delete loginControl.js

--- js/loginControl.js
+++
@@ -1,9 +0,0 @@
-function viewSignUp() {
-    document.getElementById("signup").style.display = "block";
-    document.getElementById("login").style.display = "none";
-}
-
-function viewLogin() {
-    document.getElementById("signup").style.display = "none";
-    document.getElementById("login").style.display = "block";
-}
